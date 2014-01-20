646d51a3491bc9442c8cc2278b234ac520c96ffa
ileinone <ilkka.h.leinonen@gmail.com>
2014-01-20 15:41:04 +0200

    Added documentation

--- open-infinity-identity-provisioning/open-infinity-identity-provisioning-bpmn/src/main/java/org/openinfinity/sso/identityprovisioning/bpmn/ProcessEngineBridge.java
+++ open-infinity-identity-provisioning/open-infinity-identity-provisioning-bpmn/src/main/java/org/openinfinity/sso/identityprovisioning/bpmn/ProcessEngineBridge.java
@@ -1,8 +1,30 @@
+/*
+ * Copyright (c) 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openinfinity.sso.identityprovisioning.bpmn;
 
 import java.util.Collection;
 import java.util.Map;
 
+/**
+ * Bridge interface wraps and simplifys usage for BPMN- or workflow -engine.
+ * 
+ * @author Ilkka Leinonen
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public interface ProcessEngineBridge {
 	
 	String startProcess(String processDefinitionKey, Map<String, Object> variables);
--- open-infinity-identity-provisioning/open-infinity-identity-provisioning-common/src/main/java/org/openinfinity/sso/identityprovisioning/api/IdentityProvisioningAspect.java
+++ open-infinity-identity-provisioning/open-infinity-identity-provisioning-common/src/main/java/org/openinfinity/sso/identityprovisioning/api/IdentityProvisioningAspect.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openinfinity.sso.identityprovisioning.api;
 
 import org.aspectj.lang.annotation.Aspect;
@@ -11,6 +26,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+/**
+ * Identity provisioning aspect for injecting identity to the underlying system by using IdentityProvisioningBridge API.
+ * 
+ * @author Ilkka Leinonen
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @Aspect
 public class IdentityProvisioningAspect {
 
@@ -28,7 +50,6 @@ public class IdentityProvisioningAspect {
 	@Before(value="anyIdentityProvisioningMethod() && @annotation(identityProvisioning)")
 	public void provision(IdentityProvisioning userProvisioning) {
 		long startTime = System.currentTimeMillis();
-		System.out.println("################################### stdaasdfasdfadsfarting");
 		LOGGER.debug("Starting provisioning of the Identity.");
 		SecurityContext securityContext = SecurityContextHolder.getContext();
 		Authentication authentication = securityContext.getAuthentication();
