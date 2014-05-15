2c9290790872e9e3433e523496b3b20d05781ba7
Vedran Bartonicek <vbartoni@gmail.com>
2014-05-15 14:05:20 +0300

    Logging added

---
+++ oauth/authServer/src/test/resources/sql/populate.sql
@@ -0,0 +1,5 @@
+insert into oauth_client_details (client_id, resource_ids, client_secret, scope, authorized_grant_types, authorities) /
+values('cleintCC', 'resourceCC', 'iwouldnttellyouifiknew', 'read', 'client_credentials', 'ROLE_CLIENT');
+
+insert into oauth_client_details (client_id, resource_ids, client_secret, scope, authorized_grant_types, authorities) /
+values('clientAC', 'resourceAC', 'secret', 'read', 'authorization_code', 'ROLE_USER');
